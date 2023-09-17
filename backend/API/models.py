871dd40dce8ffce3afc6298c1303f6ca957ede0c
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2023-09-16 22:30:18 -0300

    criar metodos de print para models

--- backend/API/models.py
+++ backend/API/models.py
@@ -9,22 +9,57 @@ class Usuario(models.Model):
     cpf = models.CharField(max_length=14)
     tipo = models.CharField(max_length=10)
     
+    def __str__(self) -> str:
+        texto = f'Nome: {self.nome}\n'
+        texto += f'Email: {self.email}\n'
+        texto += f'Senha: {self.senha}\n'
+        texto += f'Data Nascimento: {self.data_nascimento}\n'
+        texto += f'CPF: {self.cpf}\n'
+        texto += f'Tipo Usuário: {self.tipo}\n'
+        
+        return texto
+    
 class Cliente(models.Model):
     usuario_id = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     endereco = models.CharField(max_length=200)
     cidade = models.CharField(max_length=50)
     estado = models.CharField(max_length=2)
+    
+    def __str__(self) -> str:
+        texto = f'{Usuario.objects.get(pk=self.usuario_id)}\n'
+        texto += f'Endereço: {self.endereco}\n'
+        texto += f'Cidade: {self.cidade}\n'
+        texto += f'Estado: {self.estado}\n'
+        
+        return texto
 
 class Empresa(models.Model):
     nome = models.CharField(max_length=200)
     cnpj = models.CharField(max_length=18)
     
+    def __str__(self) -> str:
+        texto = f'Nome Fantasia: {self.nome}\n'
+        texto += f'CNPJ: {self.cnpj}\n'
+        
+        return texto
+    
 class Prestador(models.Model):
     usuario_id = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     empresa_id = models.ForeignKey(Empresa, on_delete=models.CASCADE)
     
+    def __str__(self) -> str:
+        texto = f'{Usuario.objects.get(pk=self.usuario_id)}\n'
+        texto += f'{Empresa.objects.get(pk=self.empresa_id)}\n'
+        
+        return texto
+    
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        texto = f'Categoria: {self.nome}\n'
+        
+        return texto
     
 class Servico(models.Model):
     id_prestador = models.ForeignKey(Prestador, on_delete=models.CASCADE)
@@ -32,9 +67,26 @@ class Servico(models.Model):
     preco = models.FloatField()
     duracao = models.DurationField()
     
+    def __str__(self) -> str:
+        texto = f'Nome Prestador: {Usuario.objects.get(pk=Prestador.objects.get(pk=self.id_prestador).usuario_id).nome}\n'
+        texto += f'Categoria: {Categoria.objects.get(pk=self.id_categoria).nome}\n'
+        texto += f'Preço: {self.preco}'
+        texto += f'Duração: {self.duracao}'
+        
+        return texto
+    
 class Agendamento(models.Model):
     cliente_id = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     servico_id = models.ForeignKey(Servico, on_delete=models.CASCADE)
     horario_inicio = models.DateTimeField()
     horario_fim = models.DateTimeField()
     observacoes = models.TextField()
+    
+    def __str__(self) -> str:
+        texto = f'Nome Cliente: {Usuario.objects.get(pk=Cliente.objects.get(pk=self.cliente_id).usuario_id).nome}\n'
+        texto += f'Nome Prestador: {Usuario.objects.get(pk=Cliente.objects.get(pk=self.cliente_id).usuario_id).nome}\n'
+        texto += f'Horário Início: {self.horario_inicio}\n'
+        texto += f'Horário Fim: {self.horario_fim}\n'
+        texto += f'Observações: {self.observacoes}\n'
+        
+        return texto
