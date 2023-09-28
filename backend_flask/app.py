ed1b726214ea444ed2c6f166f47a17dec2f34592
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2023-09-28 11:53:33 -0300

    começar migração flask

--- backend_flask/app.py
+++ backend_flask/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, session
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
 
 app = Flask(__name__)
 app.secret_key = b'ricardo_lanches_de_oliveira'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
 
