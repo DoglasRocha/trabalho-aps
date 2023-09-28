697377f7ba8d09bfcd623b60cdf44dd0d18a03c3
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2023-09-28 17:38:35 -0300

    criar rota de get_clientes

--- backend_flask/app.py
+++ backend_flask/app.py
@@ -1,12 +1,22 @@
 from flask import Flask, request, session
-import db
+from db import database
 from models import *
 from datetime import date
 
 app = Flask(__name__)
 app.secret_key = b'ricardo_lanches_de_oliveira'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
-db.database.init_app(app)
+database.init_app(app)
+
+@app.get('/API/usuarios/all')
+def get_usuarios() -> dict:
+    usuarios = database.session.execute(
+        database.select(Usuario)
+    ).scalars().all()
+    
+    return {"dados": [
+        usuario.get_dict() for usuario in usuarios
+    ]}
 
 @app.post('/API/clientes/create')
 def create_cliente() -> dict:
@@ -19,8 +29,8 @@ def create_cliente() -> dict:
         atributos_usuario['data_nascimento'] = date.fromisoformat(atributos_usuario['data_nascimento'])
         novo_usuario = Usuario(**atributos_usuario)
         # salva usuario no banco de dados
-        db.database.session.add(novo_usuario)
-        db.database.session.commit()
+        database.session.add(novo_usuario)
+        database.session.commit()
         
         # cria cliente
         atributos_cliente = Cliente.filtra_atributos_dicionario(dados)
@@ -28,8 +38,8 @@ def create_cliente() -> dict:
         novo_cliente.usuario_id = novo_usuario.id
 
         # salva cliente no banco de dados
-        db.database.session.add(novo_cliente)
-        db.database.session.commit()
+        database.session.add(novo_cliente)
+        database.session.commit()
         
         return {
             'usuario': novo_usuario.get_dict(),
